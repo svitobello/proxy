d1d8baf6d35ffff649a3974c930f3dbcef831984
svitobello <74673560+svitobello@users.noreply.github.com>
2021-04-12 13:05:04 +0200

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -28,7 +28,7 @@ function FindProxyForURL(url, host) {
         shExpMatch(url, "*10\.31\.*")
        )
     {
-        return "PROXY 10.41.2.10:1199";
+        return "PROXY 127.0.0.1:9090";
     }
     
 // If URL has no dots in host name, send traffic direct.
